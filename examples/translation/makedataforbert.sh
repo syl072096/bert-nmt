3145733a2cea197105466810eda5ff67dad5f1d8
syl072096 <64892499+syl072096@users.noreply.github.com>
2020-10-16 17:54:34 +0800

    makedataforbert.sh

--- examples/translation/makedataforbert.sh
+++ examples/translation/makedataforbert.sh
@@ -4,6 +4,6 @@ echo "src lng $lng"
 for sub  in train valid test
 do
     sed -r 's/(@@ )|(@@ ?$)//g' ${sub}.${lng} > ${sub}.bert.${lng}.tok
-    ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
+    /kaggle/working/bert-nmt/examples/translation/mosesdecoder/scripts/tokenizer/detokenizer.perl $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
     rm ${sub}.bert.${lng}.tok
 done
